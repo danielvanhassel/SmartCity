34d39baf296420e6139da3f9fb077eb45491f4a3
danielvanhassel <danielvanhassel@ziggo.nl>
2025-11-24 13:16:57 +0100

    added option 1,3,4 and 6 working, also changed typo in csv file

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 import csv
+import os
 movieFile = open("BF.csv", "r", encoding="utf-8")
 reader = csv.DictReader(movieFile, delimiter=";")
 movieList = list(reader)
@@ -6,6 +7,7 @@ movieList = list(reader)
 isRunning = True
 
 while isRunning:
+    os.system("cls")
     try:
         userOption = input("Kies een optie: \n" \
         "1. Gemiddeld aantal keer uitgeleend\n" \
@@ -18,22 +20,36 @@ while isRunning:
         print("U heeft een fout nummer ingevoerd")
 
     if userOption == "1":
-        print("1")
+        totalRented = 0
+        for movie in movieList:
+            totalRented += int(movie["RentedAmount2020"])
+        averageRented = totalRented / len(movieList)
+        print(f"gemiddel aantal keer uitgeleend: {averageRented}")
         
     elif userOption == "2":
         NotImplemented
 
     elif userOption == "3":
-        NotImplemented
+        for movie in movieList:
+            runtime = int(movie["durationMins"])
+            if runtime >= 180:
+                print(movie["movieTitle"])
 
     elif userOption == "4":
-        NotImplemented
+        for movie in movieList:
+            if movie["genre"] == "Thriller":
+                print(movie["movieTitle"])
 
     elif userOption == "5":
         NotImplemented
 
     elif userOption == "6":
-        NotImplemented
+        selectedGenre = input("van welk genre wilt u film bekijken? ").lower()
+        selectedYear = input("Van welk jaar wilt u films bekijken? ")
+        for movie in movieList:
+            if selectedGenre == movie["genre"].lower():
+                if selectedYear == movie["releaseYear"]:
+                    print(movie["movieTitle"])
 
     else:
         print("Uw heeft een incorrect getal ingevoerd")
@@ -41,4 +57,5 @@ while isRunning:
     userContinue = input("Wilt u doorgaan? Typ X op te stoppen ").lower()
     if userContinue == "x":
         print("Bye!")
+        os.system("cls")
         isRunning = False
