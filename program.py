4623d13d47b25539fba7e3921e075a4d8dc74b61
danielvanhassel <danielvanhassel@ziggo.nl>
2025-11-26 16:33:41 +0100

    added options 2 and 5 DONE BY Tijs Ossenblok

--- program.py
+++ program.py
@@ -20,6 +20,7 @@ while isRunning:
         print("U heeft een fout nummer ingevoerd")
 
     if userOption == "1":
+        os.system("cls")
         totalRented = 0
         for movie in movieList:
             totalRented += int(movie["RentedAmount2020"])
@@ -27,23 +28,56 @@ while isRunning:
         print(f"gemiddel aantal keer uitgeleend: {averageRented}")
         
     elif userOption == "2":
-        NotImplemented
+        os.system("cls")
+        totalRentedGenre = 0
+        countGenre = 0
+        selectedGenre = input("Van welk genre wilt u het gemiddelde weten? ").lower()
+        for movie in movieList:
+            if selectedGenre == movie["genre"].lower():
+                totalRentedGenre += int(movie["RentedAmount2020"])
+                countGenre += 1
+        if countGenre > 0:
+            averageRentedGenre = totalRentedGenre / len(movieList)
+            print(f"gemiddeld aantal keer uitgeleend voor genre {selectedGenre}: {averageRentedGenre}")
 
     elif userOption == "3":
+        os.system("cls")
         for movie in movieList:
             runtime = int(movie["durationMins"])
             if runtime >= 180:
                 print(movie["movieTitle"])
 
     elif userOption == "4":
+        os.system("cls")
         for movie in movieList:
             if movie["genre"] == "Thriller":
                 print(movie["movieTitle"])
 
     elif userOption == "5":
-        NotImplemented
+        os.system("cls")
+        def rented_amount(movie):
+            value = movie.get("RentedAmount2020", "0")
+            if value is None:
+                return 0
+            try:
+                return int(str(value).replace(",", ""))
+            except Exception:
+                try:
+                    return int(float(value))
+                except Exception:
+                    return 0
+
+        sortedMovies = sorted(movieList, key=rented_amount, reverse=True)
+        top10 = sortedMovies[:10]
+        print("Top 10 meest uitgeleende films:")
+        for i, movie in enumerate(top10, start=1):
+            title = movie.get("movieTitle", "Onbekend")
+            amount = rented_amount(movie)
+            print(f"{i}. {title} - {amount}")
+                
 
     elif userOption == "6":
+        os.system("cls")
         selectedGenre = input("van welk genre wilt u film bekijken? ").lower()
         selectedYear = input("Van welk jaar wilt u films bekijken? ")
         for movie in movieList:
